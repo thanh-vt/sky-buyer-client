1d56f8cbea35a1cba7f72e310038a19a5bdb7732
thanhvt <thanhvt@vissoft.vn>
2021-10-24 20:04:38 +0700

    update eslint + package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, {ReactElement, useCallback, useState} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {AppHeader} from './layout/header/AppHeader';
+import {decrement, increment} from './store/counterSlice';
+import {RootState} from './store';
+import {useDispatch, useSelector} from 'react-redux';
 
-function App() {
+function App(): ReactElement {
+  const [title, setTitle] = useState('ThanhVT');
+  const setTitleFunc = useCallback(() => {
+    setTitle((document.getElementById('titleInput') as HTMLInputElement).value);
+  }, []);
+  const count = useSelector((state: RootState) => state.counter.value);
+  const theme = useSelector((state: RootState) => state.theme.value);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme}`}>
+      <AppHeader/>
+      <div className="App-container">
+        <div className="App-main">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <div className="bg-danger" style={{width: '500px', height: '1000px'}}/>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+              Learn React <br/>
+              Owner: {title} <br/>
+              Count: {count}
+          </a>
+
+          <input id={'titleInput'} type="text"/>
+          <button onClick={setTitleFunc}>Set title</button>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+              Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+              Decrement
+          </button>
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ src/shared/switch/Switch.tsx
@@ -0,0 +1,21 @@
+import './Switch.scope.scss';
+import {ChangeEvent, ReactElement, useCallback, useState} from 'react';
+
+export interface SwitchProps {
+  onChange: (val: boolean) => void;
+}
+
+export function Switch(props: SwitchProps): ReactElement {
+  const [toggled, setToggled] = useState(false);
+  const toggle = useCallback((evt: ChangeEvent<HTMLInputElement>) => {
+    const val = evt.target.checked;
+    setToggled(val);
+    props.onChange(val);
+  }, [props]);
+  return (
+    <label className="switch">
+      <input type="checkbox" checked={toggled} onChange={evt => toggle(evt)}/>
+      <span className="slider round"/>
+    </label>
+  );
+}
